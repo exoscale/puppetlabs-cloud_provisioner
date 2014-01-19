b90dfdda29bea453cf829c5fc883aa5c25dec92c
Antoine Coetsier <acr@veltigroup.com>
2014-01-19 11:12:54 +0100

    Update node_cloudstack.rb

--- lib/puppet/face/node_cloudstack.rb
+++ lib/puppet/face/node_cloudstack.rb
@@ -4,9 +4,9 @@ Puppet::Face.define(:node_cloudstack, '0.0.1') do
   copyright "Puppet Labs", 2011 .. 2013
   license   "Apache 2 license; see COPYING"
 
-  summary "View and manage Amazon AWS EC2 nodes."
+  summary "View and manage CloudStack based nodes."
   description <<-'EOT'
-    This subcommand provides a command line interface to work with Amazon EC2
+    This subcommand provides a command line interface to work with CloudStack
     machine instances.  The goal of these actions is to easily create new
     machines, install Puppet onto them, and tear them down when they're no longer
     required.
